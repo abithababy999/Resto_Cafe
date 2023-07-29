c719ff30a381e23f3d81bcfc10829242a0236e95
NiranjRajesh <248753@ust.com>
2023-07-29 14:23:21 +0530

    removed lombok

--- Backend/auth-service/src/main/java/com/resto/authservice/dto/UserRegistrationRequest.java
+++ Backend/auth-service/src/main/java/com/resto/authservice/dto/UserRegistrationRequest.java
@@ -1,19 +1,59 @@
 package com.resto.authservice.dto;
 
-
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class UserRegistrationRequest {
 	    private String userName;
         private String firstName;
 	    private String lastName;
 	    private String password;
 	    private String phoneNumber;
+	    
+	    public UserRegistrationRequest() {
+	    	
+	    }
+		public UserRegistrationRequest(String userName, String firstName, String lastName, String password,
+				String phoneNumber) {
+			super();
+			this.userName = userName;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.password = password;
+			this.phoneNumber = phoneNumber;
+		}
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getPhoneNumber() {
+			return phoneNumber;
+		}
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+		
 
 }
--- Backend/auth-service/src/main/java/com/resto/authservice/model/User.java
+++ Backend/auth-service/src/main/java/com/resto/authservice/model/User.java
@@ -7,8 +7,7 @@ import javax.persistence.*;
 
 import lombok.Data;
 
-@Data
-@Entity
+
 public class User {
 	
 	@Id
@@ -16,11 +15,70 @@ public class User {
 	private long userid;
 	@Column(unique = true)
 	private String userName;
-  
-	private String firstName;
+    private String firstName;
 	private String lastName;
 	private String phoneNumber;
 	private String password;
     private String role;
+    
+    public User() {
+    	
+    }
+    
+	public User(long userid, String userName, String firstName, String lastName, String phoneNumber, String password,
+			String role) {
+		super();
+		this.userid = userid;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
+		this.role = role;
+	}
+	public long getUserid() {
+		return userid;
+	}
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
 
 }
