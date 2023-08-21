29a3d969c0c8ce1ddab0ce3ff7857b2c693586a0
niranjraj <niranjrajesh7007@gmail.com>
2023-08-21 10:48:00 +0530

    frontend starter added

---
+++ Frontend/resto-app/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <h3>
+      hello world;
+    </h3>
+    </main>
+  )
+}
